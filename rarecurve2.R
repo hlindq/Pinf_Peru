ed666d659a53111dc17d6e8869b696dadc2945bb
hlindq <54033992+hlindq@users.noreply.github.com>
2019-08-15 16:08:15 -0500

    Add files via upload

---
+++ rarecurve2.R
@@ -0,0 +1,46 @@
+rarecurve2 <- function (x, step = 1, sample, xlab = "Sample Size", ylab = "Species", 
+          label = TRUE,selected.labels, col, lty, ...) 
+{
+  x <- as.matrix(x)
+  if (!identical(all.equal(x, round(x)), TRUE)) 
+    stop("function accepts only integers (counts)")
+  if (missing(col)) 
+    col <- par("col")
+  if (missing(lty)) 
+    lty <- par("lty")
+  tot <- rowSums(x)
+  S <- specnumber(x)
+  if (any(S <= 0)) {
+    message("empty rows removed")
+    x <- x[S > 0, , drop = FALSE]
+    tot <- tot[S > 0]
+    S <- S[S > 0]
+  }
+  nr <- nrow(x)
+  col <- rep(col, length.out = nr)
+  lty <- rep(lty, length.out = nr)
+  out <- lapply(seq_len(nr), function(i) {
+    n <- seq(1, tot[i], by = step)
+    if (n[length(n)] != tot[i]) 
+      n <- c(n, tot[i])
+    drop(rarefy(x[i, ], n))
+  })
+  Nmax <- sapply(out, function(x) max(attr(x, "Subsample")))
+  Smax <- sapply(out, max)
+  plot(c(1, max(Nmax)), c(1, max(Smax)), xlab = xlab, ylab = ylab, 
+       type = "n", ...)
+  if (!missing(sample)) {
+    #abline(v = sample)
+    rare <- sapply(out, function(z) approx(x = attr(z, "Subsample"), 
+                                           y = z, xout = sample, rule = 1)$y)
+    #abline(h = rare, lwd = 0.5)
+  }
+  for (ln in seq_along(out)) {
+    N <- attr(out[[ln]], "Subsample")
+    lines(N, out[[ln]], col = col[ln], lty = lty[ln], ...)
+  }
+  if (label) {
+    ordilabel(cbind(tot, S), labels = rownames(x),select = selected.labels,  ...)
+  }
+  invisible(out)
+}
